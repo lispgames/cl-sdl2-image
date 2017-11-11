38dc078e27a0156082257ee1d4f243a419c60995
Kim Lidström <kim@dxtr.im>
2017-11-11 18:30:10 +0100

    Link with SDL2_image on openbsd

--- src/library.lisp
+++ src/library.lisp
@@ -4,6 +4,7 @@
   ;; I have no idea if this is the correct framework, file an issue
   ;; and let me know!
   (:darwin (:or (:framework "SDL2_image") (:default "libSDL2_image")))
+  (:openbsd "SDL2_image")
   (:unix (:or "libSDL2_image-2.0.so.0" "libSDL2_image"))
   (:windows "SDL2_image.dll")
   (t (:default "libSDL2_image")))
