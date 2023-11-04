792cbbdfc34c726884b8a5c5880e6f268cd7408f
Matteo Landi <matteo@matteolandi.net>
2023-11-04 08:15:02 +0100

    Parse C_INCLUDE_PATH and add its parts as :SYSINCLUDES
    This gives users a little bit of flexibility and allow them to generate
    specs in non-standard environments (e.g. a nix shell), without changing
    the autowrap definition.

--- src/autowrap.lisp
+++ src/autowrap.lisp
@@ -6,9 +6,14 @@
   :spec-path '(sdl2-image autowrap-spec)
   :exclude-sources ("/usr/local/lib/clang/([^/]*)/include/(?!stddef.h)"
                     "/usr/include/"
+                    #+darwin "/opt/homebrew/include/SDL2"
                     "/usr/include/arm-linux-gnueabihf"
                     "/usr/local/include/SDL2")
   :include-sources ("SDL_image.h")
+  :sysincludes (cl:append
+                 #+darwin '("/opt/homebrew/include")
+                 (cl:if (uiop:getenv "C_INCLUDE_PATH")
+                   (uiop:split-string (uiop:getenv "C_INCLUDE_PATH") :separator ":")))
   :exclude-constants ("^__")
   :symbol-exceptions (("SDL_RWops" . "SDL-RWOPS"))
   :no-accessors cl:t
