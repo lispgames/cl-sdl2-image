b4b06a0edb438c39ce635acd2f3bd61deee4ef3b
Thomas Fitzsimmons <fitzsim@fitzsim.org>
2024-09-14 08:01:25 -0400

    Update in library.lisp for OpenBSD needed (issue#12)
    This patch is from GitHub user "rpx99", but I do not have their email
    address to give them authorship.

--- src/library.lisp
+++ src/library.lisp
@@ -4,7 +4,7 @@
   ;; I have no idea if this is the correct framework, file an issue
   ;; and let me know!
   (:darwin (:or (:framework "SDL2_image") (:default "libSDL2_image")))
-  (:unix (:or "libSDL2_image-2.0.so.0" "libSDL2_image"))
+  (:unix (:or "libSDL2_image-2.0.so.0" "libSDL2_image" "libSDL2_image.so.1.1"))
   (:windows "SDL2_image.dll")
   (t (:default "libSDL2_image")))
 
